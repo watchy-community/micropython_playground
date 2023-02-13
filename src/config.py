e76724a84575f9113508e6f939248aef61d27322
Dustin Rowland <lee.rowland@gmail.com>
2023-02-13 10:03:25 -0600

    move dict to utils, move options to config

--- src/config.py
+++ src/config.py
@@ -2,7 +2,14 @@
 
 Various configuration options.
 """
+# False allows Deepsleep to run, True prevents Deepsleep
+DEBUG = False
 
+# Numerical timezone offset
+timeZone = -6
+
+# A list of tuples
+# tuple should be SSID then PSK as strings
 trustedWiFi = [
     # ('ssid', 'psk')
     ('ka-tet', '7f4878BAb'),
---
+++ src/utils.py
@@ -0,0 +1,30 @@
+"""src/utils.py.
+
+Reference dictionaries, shared functions, and anything used by but not
+necessarily apart of the Watchy class.
+"""
+
+weekDays = {
+    0: 'Mon',
+    1: 'Tue',
+    2: 'Wed',
+    3: 'Thu',
+    4: 'Fri',
+    5: 'Sat',
+    6: 'Sun'
+}
+
+monthNames = {
+    0: 'Jan',
+    1: 'Feb',
+    2: 'Mar',
+    3: 'Apr',
+    4: 'May',
+    5: 'Jun',
+    6: 'Jul',
+    7: 'Aug',
+    8: 'Sep',
+    9: 'Oct',
+    10: 'Nov',
+    11: 'Dec'
+}
