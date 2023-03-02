46353b93bfd65ecbecf9420cbf2aeaef364dbdf7
Dustin Rowland <lee.rowland@gmail.com>
2023-03-02 16:25:41 -0600

    change weather to read/write file

--- src/utils.py
+++ src/utils.py
@@ -39,6 +39,34 @@ monthNames = {
     11: 'Dec'
 }
 
+weatherCondition = {
+    0: 'A',   # Clear Sky
+    1: 'A',   # Mainly clear
+    2: 'B',   # Partly cloudy
+    3: 'B',   # Overcast
+    45: 'F',  # Fog
+    48: 'F',  # Depositing rime fog
+    51: 'J',  # Drizzle light
+    53: 'J',  # Drizzle moderate
+    55: 'J',  # Drizzle dense
+    56: 'S',  # Freezing drizzle light
+    57: 'S',  # Freezing drizzle dense
+    61: 'T',  # Rain slight
+    63: 'T',  # Rain moderate
+    65: 'T',  # Rain heavy
+    66: 'S',  # Freezing rain light
+    67: 'S',  # Freezing rain heavy
+    71: 'S',  # Snow fall light
+    73: 'S',  # Snow fall moderate
+    75: 'S',  # Snow fall heavy
+    77: 'S',  # Snow grains
+    80: 'T',  # Rain showers light
+    81: 'T',  # Rain showers moderate
+    82: 'T',  # Rain showers violent
+    85: 'S',  # Snow showers slight
+    86: 'S'   # Snow showers heavy
+}
+
 
 def check_weather():
     """Get weather from Open-Meteo.com."""
@@ -49,10 +77,10 @@ def check_weather():
     print('Checking weather updates')
     try:
         response = get(apiUrl, headers=headers)
+        with open('weather.json', 'w') as file:
+            file.write(response.content)
     except OSError as exc:
         if exc.errno == errno.ETIMEDOUT:
             print('Connection to weather API timed out')
         else:
             print('Unknown API error')
-        return {}
-    return response.json()
--- src/watchy.py
+++ src/watchy.py
@@ -21,13 +21,14 @@ from machine import (
     deepsleep
 )
 import errno
+import json
 import assets.fonts.monocraft_48 as monocraft_48
 import assets.fonts.monocraft_24 as monocraft_24
 import assets.fonts.weather_24 as weather_24
 from lib.display import Display
 from lib.pcf8563 import PCF8563
 from src.config import trustedWiFi, timeZone, DEBUG
-from src.utils import monthNames, weekDays, check_weather
+from src.utils import monthNames, weekDays, check_weather, weatherCondition
 from src.constants import (
     BTN_MENU,
     BTN_BACK,
@@ -77,9 +78,6 @@ class Watchy:
         i2c = SoftI2C(sda=self.pin_rtcsda, scl=self.pin_rtcscl)
         self.rtc = PCF8563(i2c)
 
-        # placeholder for weather
-        self.weather = {}
-
         self.init_interrupts()
         self.handle_wakeup()
 
@@ -115,7 +113,7 @@ class Watchy:
                 print('4th hour update')
                 self.check_network()
                 self.check_ntptime()
-                self.weather = check_weather()
+                check_weather()
             # run every minute
             self.display_watchface()
             self.set_rtc_interrupt(minutes + 1)
@@ -125,7 +123,7 @@ class Watchy:
             # the lines below are testing until rtc_int/timers are fixed
             self.check_network()
             self.check_ntptime()
-            self.weather = check_weather()
+            check_weather()
             self.display_watchface()
             self.set_rtc_interrupt(minutes + 1)
         else:
@@ -148,44 +146,39 @@ class Watchy:
         if len(str(minutes)) == 1:
             minutes = f'0{minutes}'
 
-        if self.weather != {}:
-            temp = str(round(self.weather['current_weather']['temperature']))
-        else:
-            temp = 'na'
-        
+        with open('weather.json', 'r') as file:
+            weather = json.load(file)
+
+        temp = str(round(weather['current_weather']['temperature']))
+        weathercode = weather['current_weather']['weathercode']
+
         # Top Row
         self.display.display_text(
             f'{hours}:{minutes}',
             10, 15, monocraft_48, WHITE, BLACK
         )
         # Second Row
-        #self.display.display_text(
-        #    '0123456789',
-        #    10, 74, monocraft_24, WHITE, BLACK
-        #)
+#        self.display.display_text(
+#            '0123456789',
+#            10, 74, monocraft_24, WHITE, BLACK
+#        )
         # Third Row
-        #self.display.display_text(
-        #    ',/[]:;0123',
-        #    10, 106, weather_24, WHITE, BLACK
-        #)
+#        self.display.display_text(
+#            ',/[]:;0123',
+#            10, 106, weather_24, WHITE, BLACK
+#        )
         # Fourth Row / Weather
         self.display.display_text(
             '/',
             10, 138, weather_24, WHITE, BLACK
         )
-        if temp == 'na':
-            self.display.display_text(
-                ',',
-                25, 138, weather_24, WHITE, BLACK
-            )
-        else:
-            self.display.display_text(
-                temp,
-                25, 138, monocraft_24, WHITE, BLACK
-            )
         self.display.display_text(
-            '}',
-            63, 133, weather_24, WHITE, BLACK
+            temp,
+            25, 138, monocraft_24, WHITE, BLACK
+        )
+        self.display.display_text(
+            f'{weatherCondition[weathercode]}',
+            70, 138, weather_24, WHITE, BLACK
         )
         # Bottom Row
         self.display.display_text(
