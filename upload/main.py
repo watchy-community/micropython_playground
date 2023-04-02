f131bb7788804fd96a8cef371e0d956ec729820f
firedrow <lee.rowland@gmail.com>
2023-04-02 13:19:06 -0500

    remove async in main, not used

--- upload/main.py
+++ upload/main.py
@@ -3,13 +3,13 @@
 Documenting and testing Watchy ESP32 v2 hardware.
 """
 
-import uasyncio as asyncio
 from src.watchy import Watchy
 
 
-async def main():
+def main():
     """Initialize the Watchy class object."""
     w = Watchy()
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    main()
