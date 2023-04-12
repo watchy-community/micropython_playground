915fafacdca0d903f4f0175f67b7d2f150f6b8ed
firedrow <lee.rowland@gmail.com>
2023-04-11 21:06:55 -0500

    remove timeZone in config.py

--- upload/src/config.py
+++ upload/src/config.py
@@ -5,9 +5,6 @@ Various configuration options.
 # False allows Deepsleep to run, True prevents Deepsleep
 DEBUG = False
 
-# Numerical timezone offset
-timeZone = -6
-
 # A list of tuples
 # tuple should be SSID then PSK as strings
 trustedWiFi = [
